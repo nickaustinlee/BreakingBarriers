769c9fd5924321de6df6ce413b5831707386b1d8
Nick Lee <fabbuki@gmail.com>
2012-11-08 21:41:36 -0800

    sync

---
+++ fivevoltThermo/fiveVoltThermowLEDs/fiveVoltThermowLEDs.ino
@@ -0,0 +1,52 @@
+//TMP36 Pin Variables
+int sensorPin = 0; //the analog pin the TMP36's Vout (sense) pin is connected to
+                        //the resolution is 10 mV / degree centigrade with a
+                        //500 mV offset to allow for negative temperatures
+ // Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int led = 13;
+int green = 12; 
+ 
+/*
+ * setup() - this function runs once when you turn your Arduino on
+ * We initialize the serial connection with the computer
+ */
+void setup()
+{
+  Serial.begin(9600);  //Start the serial connection with the computer
+                       //to view the result open the serial monitor 
+                       
+                         // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
+  pinMode(green,OUTPUT);
+}
+ 
+void loop()                     // run over and over again
+{
+ //getting the voltage reading from the temperature sensor
+ int reading = analogRead(sensorPin);  
+ 
+ // converting that reading to voltage, for 3.3v arduino use 3.3
+ float voltage = reading * 5.0;
+ voltage /= 1024.0; 
+ 
+ // print out the voltage
+digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+digitalWrite(green, LOW);   //turn green LED off
+delay(1000); 
+Serial.print(voltage); Serial.println(" volts");
+ 
+ // now print out the temperature
+ float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+                                               //to degrees ((volatge - 500mV) times 100)
+ Serial.print(temperatureC); Serial.println(" degrees C");
+ 
+ // now convert to Fahrenheight
+ float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+ Serial.print(temperatureF); Serial.println(" degrees F");
+ 
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(green, HIGH);  //waiting a second
+ delay(1000);
+}
+
